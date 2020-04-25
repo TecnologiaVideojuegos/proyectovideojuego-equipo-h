ab08e763c9770e0997ac0426baff2f34824d960a
mfranck340 <61021717+mfranck340@users.noreply.github.com>
2020-04-25 20:41:20 +0200

    Update PC_NPCs.py

--- Classes/PC_NPCs.py
+++ Classes/PC_NPCs.py
@@ -81,26 +81,22 @@ class Player(LivingBeing):
 class Enemy(LivingBeing):
     #Cambiar medidas de la ventana por las de la Habitacion
     def __init__(self, SCREEN_WIDTH: int, SCREEN_HEIGHT: int, image: str, scale: float):
-        self.eje_x = SCREEN_WIDTH // 2
-        self.eje_y = SCREEN_HEIGHT // 2
-        self.position_x = randrange(1, self.eje_x, 1)
-        self.position_y = randrange(1, self.eje_y, 1)
+        self.eje_x = SCREEN_WIDTH
+        self.eje_y = SCREEN_HEIGHT
+        self.position_x = randrange(1, SCREEN_WIDTH, 1)
+        self.position_y = randrange(1, SCREEN_HEIGHT, 1)
         super().__init__(self.position_x, self.position_y, image, scale)
 
-    def move_enemy(self, speed, player: Player):
-        if self.position_y == player.center_y:
-            self.change_y = 0
-        elif self.center_y < player.center_y:
-            self.change_y = speed
-        else:
-            self.change_y = -speed
+    def follow_sprite(self,SPRITE_SPEED, player_sprite):
+        if self.center_y < player_sprite.center_y:
+            self.center_y += min(SPRITE_SPEED, player_sprite.center_y - self.center_y)
+        elif self.center_y > player_sprite.center_y:
+            self.center_y -= min(SPRITE_SPEED, self.center_y - player_sprite.center_y)
 
-        if self.position_x == player.center_x:
-            self.change_x = 0
-        elif self.center_x < player.center_x:
-            self.change_x = speed
-        else:
-            self.change_x = -speed
+        if self.center_x < player_sprite.center_x:
+            self.center_x += min(SPRITE_SPEED, player_sprite.center_x - self.center_x)
+        elif self.center_x > player_sprite.center_x:
+            self.center_x -= min(SPRITE_SPEED, self.center_x - player_sprite.center_x)
 
     def draw_debug(self):
         self.sprite.draw()
