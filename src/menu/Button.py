864fabb6425b52d1d370f65d95854ce85a365c8d
Axewchopy <48996124+Axewchopy@users.noreply.github.com>
2020-05-02 22:09:18 +0200

    Added first button

--- src/menu/Button.py
+++ src/menu/Button.py
@@ -2,8 +2,10 @@ import arcade
 
 
 class Button(arcade.gui.TextButton):
-    def __init__(self, center_x, center_y, width, height, text, theme):
+    def __init__(self, center_x, center_y, width, height, text, theme: arcade.gui.Theme):
         super().__init__(center_x, center_y, width, height, text, theme)
+
+        self.f_size = theme.font_size
 
     def on_press(self):
         self.pressed = True
@@ -12,4 +14,13 @@ class Button(arcade.gui.TextButton):
         self.pressed = False
 
     def draw(self):
-        super().draw_texture_theme()
+        if self.theme:
+            self.draw_texture_theme()
+        else:
+            self.draw_color_theme()
+
+        arcade.draw_text(self.text, self.center_x, self.center_y,
+                         self.font_color, font_size=self.f_size,
+                         font_name=self.font_name,
+                         width=self.width, align="center",
+                         anchor_x="center", anchor_y="center")
