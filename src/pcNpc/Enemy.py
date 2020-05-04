9977267c48596d5bc1fd28e7c0065a9829359a3b
mfranck340 <61021717+mfranck340@users.noreply.github.com>
2020-05-04 04:04:01 +0200

    Update Enemy.py

--- src/pcNpc/Enemy.py
+++ src/pcNpc/Enemy.py
@@ -11,16 +11,6 @@ class Enemy(LivingBeing):
         self.center_x = position_x
         self.center_y = position_y
 
-    def follow_sprite(self, player_sprite):
-        if self.center_y < player_sprite.center_y:
-            self.center_y += min(self.speed, player_sprite.center_y - self.center_y)
-        elif self.center_y > player_sprite.center_y:
-            self.center_y -= min(self.speed, self.center_y - player_sprite.center_y)
-
-        if self.center_x < player_sprite.center_x:
-            self.center_x += min(self.speed, player_sprite.center_x - self.center_x)
-        elif self.center_x > player_sprite.center_x:
-            self.center_x -= min(self.speed, self.center_x - player_sprite.center_x)
 
     def go_to(self, x, y):
         dx = self.center_x - x
